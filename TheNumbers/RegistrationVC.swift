79ced23663ec7160f2507de45f3c69c81372e2e4
SiarheiShchurko <Pridemc@yandex.ru>
2022-09-01 20:03:36 +0300

    Add registration VC

---
+++ TheNumbers/RegistrationVC.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationVC.swift
+//  TheNumbers
+//
+//  Created by Alinser Shchurko on 1.09.22.
+//
+
+import Foundation
+import UIKit
+
+final class RegistrationVC: UIViewController {
+    
+    @IBOutlet private weak var enterEmail: UITextField!
+    @IBOutlet private weak var enterPassword: UITextField!
+    
+    @IBOutlet private weak var signButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+    
+    @IBAction private func SignInAction() {
+        
+        
+    }
+    
+    
+}
