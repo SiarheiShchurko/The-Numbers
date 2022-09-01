4697bec3c25e7bea8bee01bb3d1160c8fb2dcf34
SiarheiShchurko <Pridemc@yandex.ru>
2022-09-01 20:04:45 +0300

    Utils page. For favorite func and others helpers

---
+++ TheNumbers/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  TheNumbers
+//
+//  Created by Alinser Shchurko on 1.09.22.
+//
+
+import Foundation
+import UIKit
+
+//MARK: Func transition between displays
+
+func transitionBetweenDisp(cl: AnyClass) {
+
+    print("")
+    
+    
+}
