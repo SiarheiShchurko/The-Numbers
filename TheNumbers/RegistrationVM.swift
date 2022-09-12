f513e1909d214574621338b1d383ada18a202412
SiarheiShchurko <Pridemc@yandex.ru>
2022-09-12 16:14:39 +0300

    Delete methods from registrationVM. Work with UI transition to VC class

--- TheNumbers/RegistrationVM.swift
+++ TheNumbers/RegistrationVM.swift
@@ -10,42 +10,13 @@ import Foundation
 import Firebase
 
 protocol RegistrationProtocol {
-    func registration(_ email: String?, _ pass: String?)
-    func signIn(_ email: String?, _ pass: String?)
+   
 }
 
-class RegistrationVM: RegistrationProtocol {
+final class RegistrationVM: RegistrationProtocol {
 
     //MARK: Registrtion func
-    func registration(_ email: String?, _ pass: String?) {
-        //Optional delete
-        guard let email = email else { return }
-        guard let pass = pass else { return }
-        
-        //Registration
-        Auth.auth().createUser(withEmail: email, password: pass) { result, error in
-            if let error = error {
-                print(error)
-            }
-            guard let result = result else { return }
-            print(result.user.uid)
-            
-        }
-    }
+
     
-    //MARK: SignIn func
-    func signIn(_ email: String?, _ pass: String?) {
-        //Optional delete
-        guard let email = email else { return }
-        guard let pass = pass else { return }
-        
-        //Enter
-        Auth.auth().signIn(withEmail: email, password: pass) { result, error in
-            if let error = error {
-                print(error)
-            }
-            guard let result = result else { return }
-            print(result.user.uid)
-        }
-    }
+
 }
