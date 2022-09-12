5b71bb0cb8c1c2933f04242b2df8f79d4ae09d0d
SiarheiShchurko <Pridemc@yandex.ru>
2022-09-12 16:16:12 +0300

    Write logOut func

--- TheNumbers/StartPage.swift
+++ TheNumbers/StartPage.swift
@@ -6,13 +6,23 @@
 //
 //                
 import UIKit
-
+import Firebase
 
 class StartPageVC: UIViewController {
-
+    @IBOutlet private weak var logOutButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
+    }
+    
+    @IBAction private func logOutAction() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error)
+        }
     }
 }
 
