e3e1af021f09814ef9836686bfa1d4a9efa6a5bd
SiarheiShchurko <Pridemc@yandex.ru>
2022-09-13 16:20:18 +0300

    alert for quit

--- TheNumbers/StartPage.swift
+++ TheNumbers/StartPage.swift
@@ -9,20 +9,32 @@ import UIKit
 import Firebase
 
 class StartPageVC: UIViewController {
+    @IBOutlet private weak var nameUser: UILabel! {
+        didSet { nameUser.text =  }
+    }
     @IBOutlet private weak var logOutButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
     }
     
     @IBAction private func logOutAction() {
-        
-        do {
-            try Auth.auth().signOut()
-        } catch {
-            print(error)
+        let alert = UIAlertController(title: "Do you want exit?", message: nil, preferredStyle: .alert)
+        let buttonYes = UIAlertAction(title: "Yes", style: .default) { _ in
+            do {
+                try Auth.auth().signOut()
+            } catch {
+                print(error)
+            }
         }
+        let buttonNo = UIAlertAction(title: "No", style: .cancel) { _ in
+            alert.dismiss(animated: true)
+        }
+        alert.addAction(buttonYes)
+        alert.addAction(buttonNo)
+        present(alert, animated: true)
     }
 }
 
