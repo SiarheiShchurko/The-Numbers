7229b9c27f4d0e7d494834f01926b12e6ce32ca6
SiarheiShchurko <Pridemc@yandex.ru>
2022-09-25 18:09:58 +0300

    Create service for registration or sign

---
+++ TheNumbers/RegistrationVC/RegService.swift
@@ -0,0 +1,58 @@
+//
+//  RegServiceVC.swift
+//  TheNumbers
+//
+//  Created by Alinser Shchurko on 25.09.22.
+//
+
+import UIKit
+import Firebase
+
+enum RegSignResult {
+    case success
+    case failure
+    case none
+}
+    
+class RegServiceVC: UIViewController {
+    
+    private var resultt: RegSignResult = .none
+    
+    func registration(name: String, email: String, pass: String, complition: @escaping (RegSignResult) -> Void) {
+        
+        //Registration
+      
+        Auth.auth().createUser(withEmail: email, password: pass) { result, error in
+            if error == nil {
+                UserDefaults.standard.set(name, forKey: email)
+                self.resultt = .success
+                complition(self.resultt)
+                self.signIn(email: email, pass: pass) { (complition) in
+                    print(complition)
+                }
+            } else {
+                self.resultt = .failure
+                complition(self.resultt)
+            }
+                }
+            }
+        
+    
+    
+    func signIn(email: String, pass: String, complition: @escaping (RegSignResult) -> Void ) {
+
+            
+            //Enter
+            Auth.auth().signIn(withEmail: email, password: pass) { result, error in
+                if error == nil {
+                    
+                    self.resultt = .success
+                    complition(self.resultt)
+                } else {
+                    self.resultt = .failure
+                    complition(self.resultt)
+                }
+            }
+        }
+
+}
