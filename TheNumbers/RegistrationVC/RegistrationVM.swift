716ffae4c98e7f908a0b2f043d75d3ab14c1a37a
SiarheiShchurko <Pridemc@yandex.ru>
2022-09-25 18:09:22 +0300

    Created ViewModel for registrationVC

---
+++ TheNumbers/RegistrationVC/RegistrationVM.swift
@@ -0,0 +1,57 @@
+//
+//  RegistrationVM.swift
+//  TheNumbers
+//
+//  Created by Alinser Shchurko on 25.09.22.
+//
+
+import UIKit
+
+protocol RegistrationVMProtocol: AnyObject {
+    
+    var update: (() -> Void)? { get set }
+    
+    var upadateError: (() -> Void)? { get set }
+    
+    func registration(name: String, email: String, pass: String)
+    
+    func signIn(email: String, pass: String)
+    
+}
+
+class RegistrationVM: RegistrationVMProtocol {
+    
+    var update: (() -> Void)?
+    
+    var upadateError: (() -> Void)?
+    
+    let regService = RegServiceVC()
+    
+    func registration(name: String, email: String, pass: String) {
+        regService.registration(name: name, email: email, pass: pass) { [ weak self ] ( complition ) in
+            switch complition {
+            case .success:
+                self?.update?()
+            case .failure:
+                self?.upadateError?()
+            case .none:
+                break
+            }
+        }
+    }
+    
+    func signIn(email: String, pass: String) {
+        regService.signIn(email: email, pass: pass) { [ weak self ] ( complition ) in
+            switch complition {
+            case .success:
+                self?.update?()
+            case .failure:
+                self?.upadateError?()
+            case .none:
+                break
+            }
+        }
+    }
+}
+ 
+
