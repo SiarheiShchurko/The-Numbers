977a3eb463fa2c1a2ed21b62228b368bf751aa69
SiarheiShchurko <Pridemc@yandex.ru>
2022-10-12 12:33:45 +0300

    Delete redundant code. Optimizate code. GCD add.

--- TheNumbers/Services/AudioPlayer.swift
+++ TheNumbers/Services/AudioPlayer.swift
@@ -25,55 +25,35 @@ class AudioPlayerVM: AudioPlayerProtocol {
     let audioPlayerService = AudioPlayerService()
     
     //Track array
-    var trackListArray: [TrackModel] = [ TrackModel(name: "FeelingGood", url: URL(fileURLWithPath: Bundle.main.path(forResource: "FeelingGood", ofType: "mp3") ?? "")), TrackModel(name: "LittlePrankster", url: URL(fileURLWithPath: Bundle.main.path(forResource: "LittlePrankster", ofType: "mp3") ?? "")), TrackModel(name: "SummerHouse", url: URL(fileURLWithPath: Bundle.main.path(forResource: "SummerHouse", ofType: "mp3") ?? "")), TrackModel(name: "TheGoodLife", url: URL(fileURLWithPath: Bundle.main.path(forResource: "TheGoodLife", ofType: "mp3") ?? "")), TrackModel(name: "FeelingGood", url: URL(fileURLWithPath: Bundle.main.path(forResource: "FeelingGood", ofType: "mp3") ?? ""))]
+    var trackListArray: [TrackModel] = [ TrackModel(name: "FeelingGood", url: URL(fileURLWithPath: Bundle.main.path(forResource: "FeelingGood", ofType: "mp3") ?? "")), TrackModel(name: "LittlePrankster", url: URL(fileURLWithPath: Bundle.main.path(forResource: "LittlePrankster", ofType: "mp3") ?? "")), TrackModel(name: "SummerHouse", url: URL(fileURLWithPath: Bundle.main.path(forResource: "SummerHouse", ofType: "mp3") ?? "")), TrackModel(name: "TheGoodLife", url: URL(fileURLWithPath: Bundle.main.path(forResource: "TheGoodLife", ofType: "mp3") ?? "")), TrackModel(name: "UpsandDowns", url: URL(fileURLWithPath: Bundle.main.path(forResource: "UpsandDowns", ofType: "mp3") ?? ""))]
   
-    
-    //Load track
-//    func loadTracksFunc() {
-//        DispatchQueue.global(qos: .utility).async {
-//            self.trackListArray.append(self.trackFirst)
-//            self.trackListArray.append(self.trackSecond)
-//            self.trackListArray.append(self.trackThird)
-//            self.trackListArray.append(self.trackFour)
-//            self.trackListArray.append(self.trackFive)
-//        }
-       
-//    }
-    
     
     //Create audio queue
     func createPlayerQueue() {
-            //self.trackListArray.removeAll()
-            //self.loadTracksFunc()
-            trackListArray.shuffle()
-            self.trackListArray.forEach { track in
+        queue.async {
+            self.trackListArray.shuffle()
+            self.trackListArray.forEach { [ weak self ] (track) in
                let asset = AVURLAsset(url: track.url)
                let item = AVPlayerItem(asset: asset)
-                self.audioPlayerService.queuePlayer.insert(item, after: self.audioPlayerService.queuePlayer.items().last)
-                print(trackListArray.count)
+                self?.audioPlayerService.queuePlayer.insert(item, after: self?.audioPlayerService.queuePlayer.items().last)
            }
+        }
+        
     }
     
     func repeatPlaylistFunc() {
         
         audioPlayerService.token = audioPlayerService.queuePlayer.observe(\.currentItem, changeHandler: {
-            [ weak self ] (player, _) in
+            [ weak self ] ( player, _ ) in
             if self?.audioPlayerService.queuePlayer.items().count == 1 {
                 self?.createPlayerQueue() }
         })
     }
-    
-    //TracksList
-//    var trackFirst = TrackModel(name: "FeelingGood", url: URL(fileURLWithPath: Bundle.main.path(forResource: "FeelingGood", ofType: "mp3") ?? ""))
-//    var trackSecond = TrackModel(name: "LittlePrankster", url: URL(fileURLWithPath: Bundle.main.path(forResource: "LittlePrankster", ofType: "mp3") ?? ""))
-//    var trackThird = TrackModel(name: "SummerHouse", url: URL(fileURLWithPath: Bundle.main.path(forResource: "SummerHouse", ofType: "mp3") ?? ""))
-//    var trackFour = TrackModel(name: "TheGoodLife", url: URL(fileURLWithPath: Bundle.main.path(forResource: "TheGoodLife", ofType: "mp3") ?? ""))
-//    var trackFive = TrackModel(name: "FeelingGood", url: URL(fileURLWithPath: Bundle.main.path(forResource: "FeelingGood", ofType: "mp3") ?? ""))
-    
 }
 
 //MARK: Service
 final class AudioPlayerService {
+    
     //queue player
    @objc var queuePlayer = AVQueuePlayer()
    var token: NSKeyValueObservation?
@@ -81,11 +61,8 @@ final class AudioPlayerService {
     
     //func play track
     func playTrack() {
-      
             queuePlayer.volume = 0.28
             queuePlayer.play()
-       
-      
     }
     
     //func play track
